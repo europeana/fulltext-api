ffb1370b7e600c0fd9439b0f54c2586dedf84e66
Patrick Ehlert <patrick.ehlert@europeana.eu>
2018-11-06 10:02:32 +0100

    Fix connecting to alternative mongo database

--- loader/src/main/java/eu/europeana/fulltext/loader/config/DataSourceConfig.java
+++ loader/src/main/java/eu/europeana/fulltext/loader/config/DataSourceConfig.java
@@ -19,12 +19,14 @@ package eu.europeana.fulltext.loader.config;
 
 import com.mongodb.MongoClient;
 import com.mongodb.MongoClientURI;
+import org.apache.logging.log4j.LogManager;
 import org.mongodb.morphia.AdvancedDatastore;
 import org.mongodb.morphia.Morphia;
 import org.springframework.boot.autoconfigure.mongo.MongoProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
+import org.springframework.util.StringUtils;
 
 /**
  * Created by luthien on 01/10/2018.
@@ -37,11 +39,15 @@ public class DataSourceConfig {
 
     @Bean
     public AdvancedDatastore datastore(MongoClient mongoClient, MongoProperties mongoProperties) {
-
-        // create the Datastore connecting to the default port on the local host
-        MongoClientURI          uri = new MongoClientURI(mongoProperties.getUri());
-        String                  defaultDatabase = uri.getDatabase();
-        final AdvancedDatastore datastore = (AdvancedDatastore) new Morphia().createDatastore(mongoClient, defaultDatabase);
+        // There can be an alternative database defined via spring.data.mongodb.database, so check if that's the case
+        String database = mongoProperties.getDatabase();
+        MongoClientURI uri = new MongoClientURI(mongoProperties.getUri());
+        if (StringUtils.isEmpty(database)) {
+            database = uri.getDatabase();
+        }
+        LogManager.getLogger(eu.europeana.fulltext.api.config.DataSourceConfig.class).
+                info("Connecting to {} Mongo database on hosts {}...", database, uri.getHosts());
+        final AdvancedDatastore datastore = (AdvancedDatastore) new Morphia().createDatastore(mongoClient, database);
         datastore.ensureIndexes();
         return datastore;
     }
