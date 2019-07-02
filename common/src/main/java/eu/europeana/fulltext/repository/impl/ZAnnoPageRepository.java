6051b2c05c82f666694f3252a6a3688f43216c0d
Luthien-in-edhil <maike.dulk@europeana.eu>
2019-07-02 15:13:19 +0200

    removed repository/impl base-crud... construct

--- common/src/main/java/eu/europeana/fulltext/repository/impl/ZAnnoPageRepository.java
+++
@@ -1,9 +0,0 @@
-package eu.europeana.fulltext.repository.impl;
-import eu.europeana.fulltext.entity.AnnoPage;
-import eu.europeana.fulltext.repository.impl.CrudRepository;
-import org.bson.types.ObjectId;
-
-/**
- * Created by luthien on 01/10/2018.
- */
-public interface ZAnnoPageRepository extends CrudRepository<AnnoPage, ObjectId> {}
--- common/src/main/java/eu/europeana/fulltext/repository/impl/ZResourceRepository.java
+++
@@ -1,9 +0,0 @@
-package eu.europeana.fulltext.repository.impl;
-
-import eu.europeana.fulltext.entity.Resource;
-import eu.europeana.fulltext.repository.impl.CrudRepository;
-
-/**
- * Created by luthien on 01/10/2018.
- */
-public interface ZResourceRepository extends CrudRepository<Resource, String> {}
