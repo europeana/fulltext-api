6e9afb294f1921d5680121737f31fa5243efe2f1
Luthien-in-edhil <maike.dulk@europeana.eu>
2019-09-24 18:04:10 +0200

    spring security IP restriction test

--- api/src/main/java/eu/europeana/fulltext/api/FTApplication.java
+++ api/src/main/java/eu/europeana/fulltext/api/FTApplication.java
@@ -8,6 +8,11 @@ import org.springframework.boot.web.servlet.support.SpringBootServletInitializer
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -86,11 +91,24 @@ public class FTApplication extends SpringBootServletInitializer {
 	}
 
     @Configuration
-	class WebConfig implements WebMvcConfigurer {
+	static class WebConfig implements WebMvcConfigurer {
         @Override
         public void addCorsMappings(CorsRegistry registry) {
             registry.addMapping("/**").allowedOrigins("*").maxAge(1000);
         }
+    }
+
+
+    @EnableWebSecurity
+    @Configuration
+    static class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+        @Override
+        protected void configure(HttpSecurity http) throws Exception {
+			http.authorizeRequests()
+				.antMatchers("/presentation/**").hasIpAddress("194.171.184.20");
+		}
+
     }
 
 }
