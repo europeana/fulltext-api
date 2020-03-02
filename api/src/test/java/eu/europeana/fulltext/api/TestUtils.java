0a431f752d1459fc55f59212579b1cf96b0b28a1
srishti <srishti.singh@europeana.eu>
2020-03-02 17:14:54 +0100

    EA-1823 : fixed Junit

--- api/src/test/java/eu/europeana/fulltext/api/TestUtils.java
+++ api/src/test/java/eu/europeana/fulltext/api/TestUtils.java
@@ -85,8 +85,8 @@ class TestUtils {
         prepareAnnotationPages();
 
         // build example AnnoPage bean with all containing entities, to mock the Repository with
-        res_1 = new Resource("res1", "de", EDMRIGHTS,  KUCKEBACKENWOLLTE, DS_ID, LCL_ID);
-        res_2 = new Resource("res2", "de", EDMRIGHTS, WUERDEJANICHTAUFGEHEN, DS_ID, LCL_ID);
+        res_1 = new Resource("res1", "de",  KUCKEBACKENWOLLTE, EDMRIGHTS, DS_ID, LCL_ID);
+        res_2 = new Resource("res2", "de", WUERDEJANICHTAUFGEHEN, EDMRIGHTS, DS_ID, LCL_ID);
         tgt_1 = new Target(60,100,30,14);
         tgt_2 = new Target(95,102,53,15);
         tgt_3 = new Target(60,96,404,19);
@@ -174,8 +174,8 @@ class TestUtils {
     }
 
     static void buildFTResources(){
-        ftres_1 = createFTResource("res1", "de", EDMRIGHTS,  KUCKEBACKENWOLLTE);
-        ftres_2 = createFTResource("res2", "de", EDMRIGHTS,  WUERDEJANICHTAUFGEHEN);
+        ftres_1 = createFTResource("res1", "de", KUCKEBACKENWOLLTE, EDMRIGHTS);
+        ftres_2 = createFTResource("res2", "de" , WUERDEJANICHTAUFGEHEN, EDMRIGHTS);
         ftres_2.setContext(MEDIA_TYPE_EDM_JSONLD);
     }
 
