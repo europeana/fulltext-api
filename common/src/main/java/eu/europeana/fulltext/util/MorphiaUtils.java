cdf8998b7312b5c49a73e6d8c384a5bb03b9f36b
Immanuel Kattey <immanuel.kattey@europeana.eu>
2020-10-13 17:32:52 +0200

    EA-2235: Make Morphia mappings backwards-compatible

--- common/src/main/java/eu/europeana/fulltext/util/MorphiaUtils.java
+++ common/src/main/java/eu/europeana/fulltext/util/MorphiaUtils.java
@@ -1,7 +1,9 @@
 package eu.europeana.fulltext.util;
 
 import dev.morphia.DeleteOptions;
+import dev.morphia.mapping.DiscriminatorFunction;
 import dev.morphia.mapping.MapperOptions;
+import dev.morphia.mapping.NamingStrategy;
 
 public class MorphiaUtils {
 
@@ -11,8 +13,10 @@ public class MorphiaUtils {
 
     public static final MapperOptions MAPPER_OPTIONS = MapperOptions
             .builder()
-            // use legacy key for backwards-compatibility
+            // use legacy settings for backwards-compatibility
             .discriminatorKey("className")
+            .discriminator(DiscriminatorFunction.className())
+            .fieldNaming(NamingStrategy.identity())
             .build();
 
     // Morphia deletes the first matching document by default. This can be used for deleting all matches
