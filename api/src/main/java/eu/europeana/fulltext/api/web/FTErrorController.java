8323d83f614051ccba09008a0ec7902ee78780a1
Immanuel Kattey <immanuel.kattey@europeana.eu>
2020-11-10 19:24:51 +0100

    EA-1868: Fix error page for browser requests

--- api/src/main/java/eu/europeana/fulltext/api/web/FTErrorController.java
+++ api/src/main/java/eu/europeana/fulltext/api/web/FTErrorController.java
@@ -1,12 +1,12 @@
 package eu.europeana.fulltext.api.web;
 
+import eu.europeana.fulltext.api.config.FTDefinitions;
 import org.springframework.boot.autoconfigure.web.servlet.error.AbstractErrorController;
 import org.springframework.boot.web.error.ErrorAttributeOptions;
 import org.springframework.boot.web.servlet.error.ErrorAttributes;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import javax.servlet.http.HttpServletRequest;
 import java.util.Map;
@@ -14,20 +14,19 @@ import java.util.Map;
 /**
  * Created by luthien on 2019-08-13.
  */
-
 @Controller
-@RequestMapping("/error")
 public class FTErrorController extends AbstractErrorController {
 
     public FTErrorController(ErrorAttributes errorAttributes) {
         super(errorAttributes);
     }
 
-    @RequestMapping
-    public ResponseEntity<Map<String, Object>> error(final HttpServletRequest request) {
-        final Map<String, Object> body = this.getErrorAttributes(request, ErrorAttributeOptions.defaults());
-        final HttpStatus status = this.getStatus(request);
-        return new ResponseEntity<>(body, status);
+
+    @RequestMapping(value = "/error", produces = {FTDefinitions.MEDIA_TYPE_JSON, FTDefinitions.MEDIA_TYPE_JSONLD})
+    @ResponseBody
+    public Map<String, Object> error(final HttpServletRequest request) {
+        return this.getErrorAttributes(request,
+                ErrorAttributeOptions.of(ErrorAttributeOptions.Include.MESSAGE));
     }
 
     @Override
