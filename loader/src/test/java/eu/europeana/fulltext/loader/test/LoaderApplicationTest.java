731a33430a44e77e933524ad90d977a727693c36
srishtisingh-eu <srishti.singh@europeana.eu>
2021-01-18 14:10:18 +0100

    EA-2242 fixed the loader junit test

--- loader/src/test/java/eu/europeana/fulltext/loader/test/LoaderApplicationTest.java
+++ loader/src/test/java/eu/europeana/fulltext/loader/test/LoaderApplicationTest.java
@@ -2,11 +2,8 @@ package eu.europeana.fulltext.loader.test;
 
 import eu.europeana.fulltext.loader.LoaderApplication;
 import org.junit.Test;
-import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
 
-@RunWith(SpringRunner.class)
 @SpringBootTest(classes = LoaderApplication.class)
 public class LoaderApplicationTest {
 
