0d862d868115e62c65634be17a2f9db7f5a49921
srishti <srishti.singh@europeana.eu>
2022-08-01 10:36:01 +0200

    EA-3039 sonarQube suggestion and our code format changes

--- indexing/src/main/java/eu/europeana/fulltext/indexing/config/DataSourceConfig.java
+++ indexing/src/main/java/eu/europeana/fulltext/indexing/config/DataSourceConfig.java
@@ -7,27 +7,22 @@ import static eu.europeana.fulltext.util.MorphiaUtils.MAPPER_OPTIONS;
 import com.mongodb.client.MongoClients;
 import dev.morphia.Datastore;
 import dev.morphia.Morphia;
+import org.apache.commons.lang3.StringUtils;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-import org.apache.solr.client.solrj.SolrClient;
 import org.apache.solr.client.solrj.impl.CloudSolrClient;
-//import org.apache.solr.client.solrj.impl.Http2SolrClient;
-import org.apache.solr.client.solrj.impl.LBHttpSolrClient;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Primary;
 import org.springframework.context.annotation.PropertySource;
 
 import java.util.Arrays;
-import java.util.List;
-import java.util.Optional;
 
 @Configuration
 @PropertySource("classpath:indexing.properties")
 @PropertySource(value = "classpath:indexing.user.properties", ignoreResourceNotFound = true)
 public class DataSourceConfig {
-  private static final Logger logger = LogManager.getLogger(DataSourceConfig.class);
+  private static final Logger LOG = LogManager.getLogger(DataSourceConfig.class);
 
   @Value("${mongo.connectionUrl}")
   private String mongoConnectionUrl;
@@ -35,37 +30,37 @@ public class DataSourceConfig {
   @Value("${mongo.fulltext.database}")
   private String fulltextDatabase;
 
-  @Value("${solr.metadata.url}")
-  private String metadataSolrUrl;
-
   @Value("${solr.fulltext.url}")
   private String fulltextSolrUrl;
-
-  @Value("${solr.fulltext.zk.url}")
-  private String fulltextZkUrl;
-
-  @Value("${solr.metadata.collection}")
-  private String metadataCollection;
 
   @Value("${solr.fulltext.collection}")
   private String fulltextCollection;
 
+  @Value("${solr.metadata.url}")
+  private String metadataSolrUrl;
+
+  @Value("${solr.metadata.collection}")
+  private String metadataCollection;
+
+  @Value("${solr.fulltext.zk.url}")
+  private String fulltextZkUrl;
+
   @Bean
   public Datastore fulltextDatastore() {
-    logger.info("Configuring fulltext database: {}", fulltextDatabase);
+    LOG.info("Configuring fulltext database: {}", fulltextDatabase);
     return Morphia.createDatastore(
         MongoClients.create(mongoConnectionUrl), fulltextDatabase, MAPPER_OPTIONS);
   }
 
   //TODO: maybe replace by metis solr utils
   /**
-   * Create metadata client from solr node directly because it is expected it will be only one node, and we need this information to get the cores
-   * @return
+   * Create metadata client from solr node directly because it is expected it will be only one node,
+   * and we need this information to get the cores
+   * @return SolrClient client for metadata collection
    */
   @Bean(METADATA_SOLR_BEAN)
   public CloudSolrClient metadataSolrClient(){
-    logger.info("Configuring metadata solr client: {}", metadataSolrUrl);
-    // new Http2SolrClient.Builder(metadataSolrUrl).build();
+    LOG.info("Configuring metadata solr client: {}", metadataSolrUrl);
     CloudSolrClient client = new CloudSolrClient.Builder(Arrays.asList(metadataSolrUrl)).build();
     client.setDefaultCollection(metadataCollection);
     return client;
@@ -73,16 +68,21 @@ public class DataSourceConfig {
 
   //TODO: maybe replace by metis solr utils
   /**
-   * Create fulltext client from solr nodes (ideally it should be created from zookeeper nodes because it is expected it will be more than one node and it is more efficient but I sometimes get problems with the connection this way
-   * @return
+   * Create fulltext client from solr nodes
+   * (ideally it should be created from zookeeper nodes because it is expected it will be more than one node
+   * and it is more efficient but I sometimes get problems with the connection this way
+   *
+   * @return Solr client for fulltext
    */
   @Bean(FULLTEXT_SOLR_BEAN)
   public CloudSolrClient fulltextSolrClient(){
-    logger.info("Configuring fulltext solr client: {}", fulltextSolrUrl);
-    //return new Http2SolrClient.Builder(fulltextSolrUrl).build();
-    //CloudSolrClient client = new CloudSolrClient.Builder(Arrays.asList(fulltextZkUrl), java.util.Optional.empty()).build(); I have trouble sometimes connecting to Zookeeper
+    LOG.info("Configuring fulltext solr client: {}", fulltextSolrUrl);
+    // There is some issue connecting with zookeeper reported by Monica
+    if (StringUtils.isNotEmpty(fulltextZkUrl)) {
+      return new CloudSolrClient.Builder(Arrays.asList(fulltextZkUrl), java.util.Optional.empty()).build();
+    }
     CloudSolrClient client = new CloudSolrClient.Builder(Arrays.asList(fulltextSolrUrl)).build();
-     client.setDefaultCollection(fulltextCollection);
+    client.setDefaultCollection(fulltextCollection);
     return  client;
   }
 }
--- indexing/src/main/java/eu/europeana/fulltext/indexing/repository/IndexingAnnoPageRepository.java
+++ indexing/src/main/java/eu/europeana/fulltext/indexing/repository/IndexingAnnoPageRepository.java
@@ -1,15 +1,10 @@
 package eu.europeana.fulltext.indexing.repository;
 
-import static dev.morphia.aggregation.experimental.expressions.Expressions.field;
-import static dev.morphia.aggregation.experimental.stages.Group.group;
-import static dev.morphia.aggregation.experimental.stages.Group.id;
 import static dev.morphia.query.experimental.filters.Filters.and;
 import static dev.morphia.query.experimental.filters.Filters.eq;
-import static dev.morphia.query.experimental.filters.Filters.exists;
 import static dev.morphia.query.experimental.filters.Filters.gt;
 import static eu.europeana.fulltext.util.MorphiaUtils.Fields.DATASET_ID;
 import static eu.europeana.fulltext.util.MorphiaUtils.Fields.DELETED;
-import static eu.europeana.fulltext.util.MorphiaUtils.Fields.DOC_ID;
 import static eu.europeana.fulltext.util.MorphiaUtils.Fields.LANGUAGE;
 import static eu.europeana.fulltext.util.MorphiaUtils.Fields.LOCAL_ID;
 import static eu.europeana.fulltext.util.MorphiaUtils.Fields.MODIFIED;
@@ -17,42 +12,38 @@ import static eu.europeana.fulltext.util.MorphiaUtils.Fields.RESOURCE;
 import static eu.europeana.fulltext.util.MorphiaUtils.Fields.TARGET_ID;
 
 import dev.morphia.Datastore;
-import dev.morphia.aggregation.experimental.stages.Projection;
-import dev.morphia.aggregation.experimental.stages.ReplaceRoot;
-import dev.morphia.aggregation.experimental.stages.Sort;
 import dev.morphia.query.FindOptions;
 import dev.morphia.query.MorphiaCursor;
 import eu.europeana.fulltext.entity.AnnoPage;
-import eu.europeana.fulltext.indexing.DataIdWrapper;
 import java.util.Date;
 import java.util.List;
+
+import eu.europeana.fulltext.indexing.DataIdWrapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
-
-import javax.xml.crypto.Data;
 
 @Repository
 public class IndexingAnnoPageRepository {
 
-  @Autowired private Datastore datastore;
-
+  @Autowired
+  private Datastore datastore;
 
   /**
    * Fetches all records modified after the specified date.
    * Returns a list containing the unique dsId and lcId combinations from modified AnnoPages.
    */
-/*  public List<DataIdWrapper> getRecordsModifiedAfter(Date date) {
-    return datastore
-        .aggregate(AnnoPage.class)
-        //.match(gt(MODIFIED, date), eq(DELETED, null)) Replacing this one by the one below because we also need to know if there were modifications
-        .match(gt(MODIFIED, date))
-        .project(Projection.project().include(DATASET_ID).include(LOCAL_ID))
-        .group(group(id().field(DATASET_ID).field(LOCAL_ID)))
-        // _id created in group stage, containing dsId and lcId. We
-        .replaceRoot(ReplaceRoot.replaceRoot(field(DOC_ID)))
-        .execute(DataIdWrapper.class)
-        .toList();
-  }*/
+// public List<DataIdWrapper> getRecordsModifiedAfter(Date date) {
+//   return datastore
+//           .aggregate(AnnoPage.class)
+//           //.match(gt(MODIFIED, date), eq(DELETED, null)) Replacing this one by the one below because we also need to know if there were modifications
+//           .match(gt(MODIFIED, date))
+//           .project(Projection.project().include(DATASET_ID).include(LOCAL_ID))
+//           .group(group(id().field(DATASET_ID).field(LOCAL_ID)))
+//           // _id created in group stage, containing dsId and lcId. We
+//           .replaceRoot(ReplaceRoot.replaceRoot(field(DOC_ID)))
+//           .execute(DataIdWrapper.class)
+//           .toList();
+// }
 
   //this seems to be more efficient if we try to get big amounts of records
   public MorphiaCursor<AnnoPage> getRecordsModifiedAfter_stream(Date date) {
