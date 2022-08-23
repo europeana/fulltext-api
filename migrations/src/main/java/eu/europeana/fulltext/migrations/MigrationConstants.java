184a671d47e909ab5020aa6f493ad682c8c2dc98
Immanuel Kattey <immanuel.kattey@europeana.eu>
2022-08-23 15:16:48 +0100

    EA-3046: Allow configuring different migration source and destination

--- migrations/src/main/java/eu/europeana/fulltext/migrations/MigrationConstants.java
+++ migrations/src/main/java/eu/europeana/fulltext/migrations/MigrationConstants.java
@@ -2,8 +2,8 @@ package eu.europeana.fulltext.migrations;
 
 public class MigrationConstants {
 
-  public static final String FULLTEXT_DATASTORE_BEAN = "fulltextDb";
-  public static final String BATCH_DATASTORE_BEAN = "batchDb";
+  public static final String FULLTEXT_SRC_DATASTORE = "fulltextSrcDataStore";
+  public static final String FULLTEXT_DEST_DATASTORE = "fulltextDestDataStore";
 
   public static final String BATCH_THREAD_EXECUTOR = "batchThreadExecutor";
 }
