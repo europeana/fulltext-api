819a0df0a305d7dd9553f17848d8378d45bc6e99
Immanuel Kattey <immanuel.kattey@europeana.eu>
2022-08-25 13:59:01 +0100

    EA-3046: Disable Spring Mongo auto configuration

--- migrations/src/main/java/eu/europeana/fulltext/migrations/MigrationApp.java
+++ migrations/src/main/java/eu/europeana/fulltext/migrations/MigrationApp.java
@@ -3,6 +3,8 @@ package eu.europeana.fulltext.migrations;
 import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.data.mongo.MongoDataAutoConfiguration;
+import org.springframework.boot.autoconfigure.mongo.MongoAutoConfiguration;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.context.ConfigurableApplicationContext;
 
@@ -11,7 +13,11 @@ import org.springframework.context.ConfigurableApplicationContext;
     scanBasePackages = "eu.europeana.fulltext",
     exclude = {
       // Disabled as we're using our own auth mechanism
-      SecurityAutoConfiguration.class
+      SecurityAutoConfiguration.class,
+
+      // disable Spring Mongo auto config
+      MongoAutoConfiguration.class,
+      MongoDataAutoConfiguration.class
     })
 public class MigrationApp {
   public static void main(String[] args) {
