e416a673dad9548686fdec6b48b5fbeffea81779
srishti <srishti.singh@europeana.eu>
2024-02-14 16:12:31 +0100

    ## Upgrade api-commons version

--- api/src/main/java/eu/europeana/fulltext/api/config/AuthorizationConfig.java
+++ api/src/main/java/eu/europeana/fulltext/api/config/AuthorizationConfig.java
@@ -1,6 +1,7 @@
 package eu.europeana.fulltext.api.config;
 
 import eu.europeana.api.commons.definitions.vocabulary.Role;
+import eu.europeana.api.commons.nosql.service.ApiWriteLockService;
 import eu.europeana.api.commons.oauth2.service.impl.EuropeanaClientDetailsService;
 import eu.europeana.api.commons.service.authorization.BaseAuthorizationService;
 import eu.europeana.fulltext.api.web.Roles;
@@ -37,6 +38,11 @@ public class AuthorizationConfig extends BaseAuthorizationService
   @Override
   protected String getApiName() {
     return appSettings.getAuthorizationApiName();
+  }
+
+  @Override
+  protected ApiWriteLockService getApiWriteLockService() {
+    return null;
   }
 
   @Override
