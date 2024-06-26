e28c894cfc79652d518dd66346f33640d931da0b
Shwetambara Nazare <nshweta90@gmail.com>
2024-06-26 23:55:33 +0200

    JDK 17 Upgrade

--- api/src/main/java/eu/europeana/fulltext/api/model/v2/AnnotationFullBodyV2.java
+++ api/src/main/java/eu/europeana/fulltext/api/model/v2/AnnotationFullBodyV2.java
@@ -1,18 +1,27 @@
 package eu.europeana.fulltext.api.model.v2;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.annotation.JsonPropertyOrder;
-import ioinformarics.oss.jackson.module.jsonld.annotation.JsonldType;
 
 /**
  * Created by luthien on 14/06/2018.
  */
-@JsonldType("oa:SpecificResource")
-@JsonPropertyOrder({"id"})
+
+@JsonPropertyOrder({"id","type"})
 public class AnnotationFullBodyV2 extends AnnotationBodyV2{
+
+    private static final String TYPE = "oa:SpecificResource";
 
     private static final long serialVersionUID = 3894020068338298481L;
 
     public AnnotationFullBodyV2(String id) {
         super(id);
     }
+
+    @Override
+    @JsonProperty("@type")
+    public String getType() {
+        return TYPE;
+    }
+
 }
